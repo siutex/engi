97e3774b2f04d9927fc5cb702fb7bfac318b0a5f
Pawel Ozog <pawel.ozog@yahoo.com>
2018-12-02 15:48:19 +0100

    ENGI-6- Fix mappings

--- frontend/src/app/hospital/hospital.ts
+++ frontend/src/app/hospital/hospital.ts
@@ -1,7 +1,7 @@
 export class Hospital {
     public address: string;
-    public hospitalName: string;
-    public city_code: string;
+    public healthCenterName: string;
+    public cityCode: string;
     constructor() {  }
 }
 
